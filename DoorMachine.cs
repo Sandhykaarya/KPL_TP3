a16ee63561fc796639c283fb53ebb1576fb747db
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-03-07 12:34:12 +0700

    menambahkan implementasi dengan state-based construction

---
+++ DoorMachine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204002
+{
+    internal class DoorMachine
+    {
+        enum Door {Terkunci, Terbuka};
+
+        public void machine()
+        {
+            Door door = Door.Terkunci;
+            String[] kondisi = { "Terkunci", "Terbuka" };
+            while(true)
+            {
+                Console.WriteLine(kondisi[(int)door]);
+                Console.Write("Masukkan instruksi : ");
+                string command = Console.ReadLine();
+                switch(door)
+                {
+                    case Door.Terkunci:
+                        if(command == "BukaPintu")
+                        {
+                            door = Door.Terbuka;
+                            Console.WriteLine("Pintu tidak terkunci");
+                            Console.WriteLine();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            door = Door.Terkunci;
+                            break;
+                        }
+                    case Door.Terbuka:
+                        if (command == "KunciPintu")
+                        {
+                            door = Door.Terkunci;
+                            Console.WriteLine("Pintu terkunci");
+                            Console.WriteLine();
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine();
+                            door = Door.Terbuka;
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,8 +8,15 @@ namespace tpmodul3_1302204002
         static void Main(string[] args)
         {
             KodePos kp = new KodePos();
+            DoorMachine dm = new DoorMachine();
 
-            Console.WriteLine("Kode Pos: " + kp.getKodePos(Console.ReadLine()));
+            // Kode Pos
+            // Console.WriteLine("Kode Pos: " + kp.getKodePos(Console.ReadLine()));
+
+            // Door Machine
+            // Ini infinite loop, tutup program saat sudah selesai digunakan
+            // Command : BukaPintu, KunciPintu
+            // dm.machine();
         }
     }
 }
