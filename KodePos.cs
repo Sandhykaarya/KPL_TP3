6effd560d3da3fddedac34bfbf7cd8d4f9618c82
Sandhykaarya <sandhykaarya@student.telkomuniversity.ac.id>
2022-03-07 11:27:41 +0700

    menambahkan implementasi dengan table-driven

---
+++ KodePos.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul3_1302204002
+{
+    internal class KodePos
+    {
+        public string getKodePos(String kelurahan)
+        {
+            switch(kelurahan)
+            {
+                case "Batununggal":
+                    return "40266";
+                case "Kujangsari":
+                    return "40287";
+                case "Mengger":
+                    return "40267";
+                case "Wates":
+                    return "40256";
+                case "Cijaura":
+                    return "40287";
+                case "Jatisari":
+                    return "40286";
+                case "Margasari":
+                    return "40286";
+                case "Sekejati":
+                    return "40286";
+                case "Kebonwaru":
+                    return "40272";
+                case "Maleer":
+                    return "40274";
+                case "Samoja":
+                    return "40273";
+                default:
+                    return "Kelurahan tidak ditemukan!";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+namespace tpmodul3_1302204002 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KodePos kp = new KodePos();
+
+            Console.WriteLine(kp.getKodePos(Console.ReadLine()));
+        }
+    }
+}
